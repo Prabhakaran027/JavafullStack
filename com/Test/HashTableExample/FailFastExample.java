fb1623744ae7a4b9725e8cb9f20e78cb05992139
Prabhakaran027 <prabhaharanarjunan@gmail.com>
2024-01-16 07:30:33 +0530

    commit

---
+++ com/Test/HashTableExample/FailFastExample.java
@@ -0,0 +1,26 @@
+package com.Test.HashTableExample;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class FailFastExample {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(15);
+
+        Iterator<Integer> iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next() == 11) {
+                list.remove(1);
+            }
+
+        }
+    }
+}
